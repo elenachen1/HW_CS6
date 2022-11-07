478b4b208722fae33b3f34bafe84ee3e0a4d1a76
LenaChen <74610301+elenachen1@users.noreply.github.com>
2022-11-07 18:31:31 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+Console.WriteLine($"Введите элементы массива размером {size}");
+ArrayNumbersFunction(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > 0)
+    {
+        count++;
+    }
+}
+
+
+Console.WriteLine($"Сумма нечетных чисел равна - {count}");
+
+void ArrayNumbersFunction(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
